27cc85d9bf4b4b69ce60f4e516286f4eaf507d79
knaecks <knaecks+github@protonmail.com>
2025-07-22 17:51:42 +0200

    Update browser-sessions.blade.php
    Removed x-filament-panels component from browser-sessions. It is not available in filament 4.

--- resources/views/livewire/browser-sessions.blade.php
+++ resources/views/livewire/browser-sessions.blade.php
@@ -1,8 +1,6 @@
 <x-filament-breezy::grid-section md=2 :title="__('filament-breezy::default.profile.browser_sessions.heading')" :description="__('filament-breezy::default.profile.browser_sessions.subheading')">
     <div class="bg-white rounded-lg shadow-sm ring-1 ring-gray-950/5 p-6 dark:bg-gray-900 dark:ring-white/10">
-        <x-filament-panels::form>
-            {{ $this->form }}
-        </x-filament-panels::form>
+        {{ $this->form }}
 
         <x-filament-actions::modals />
     </div>
